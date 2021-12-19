1a865e7d984302cdf481d026bafd0cb12500166e
gureumwoon <vfrvfr3221@gmail.com>
2021-12-20 01:09:31 +0900

    backend에서 login 기능 구현

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,14 +1,40 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["woony", "gureum", "young"],
+    psword: ["1234", "1234", "5678"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패 하셨습니다.",
+        });
+    },
 };
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
